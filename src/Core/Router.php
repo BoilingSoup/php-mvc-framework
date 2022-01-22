d5e77928cd9d56031585f41f35c24acd7aebf71c
boilingsoup <boilingsoup@protonmail.com>
2022-01-22 11:31:18 -0800

    create routing table and add some routes

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -1,5 +1,18 @@
 <?php
 
-class Router{
+declare(strict_types=1);
 
+class Router
+{
+  protected $routes = [];
+
+  public function add(string $route, array $params): void
+  {
+    $this->routes[$route] = $params;
+  }
+
+  public function getRoutes(): array
+  {
+    return $this->routes;
+  }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,18 +1,19 @@
 <?php
 
-/**
- * Front controller
- * PHP version 5.4
- */
+declare(strict_types=1);
 
 // echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
-
-/**
- * Routing
- */
 
 require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';

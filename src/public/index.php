02d00be9b818fbc3fdbe33a4271ad5f688529928
boilingsoup <boilingsoup@protonmail.com>
2022-01-22 09:48:43 -0800

    init Router class

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+/**
+ * Front controller
+ * PHP version 5.4
+ */
+
+// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);

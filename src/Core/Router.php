8749094aa1ad964cb7c80004a866b9c07dc108a0
boilingsoup <boilingsoup@protonmail.com>
2022-01-22 11:45:32 -0800

    add basic route matching

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 class Router
 {
   protected $routes = [];
+  protected $params = [];
 
   public function add(string $route, array $params): void
   {
@@ -14,5 +15,20 @@ class Router
   public function getRoutes(): array
   {
     return $this->routes;
+  }
+
+  public function match($url): bool
+  {
+    if (array_key_exists($url, $this->routes)) {
+      $this->params = $this->routes[$url];
+      return true;
+    }
+
+    return false;
+  }
+
+  public function getParams(): array
+  {
+    return $this->params;
   }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -14,6 +14,15 @@ $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 
 // Display the routing table
-echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>';
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+  echo '<pre>';
+  var_dump($router->getParams());
+  echo '</pre>';
+}
